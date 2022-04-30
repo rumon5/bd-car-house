1224213c8650b70f4c66ac8a831030df1b2ceaa0
rumon5 <mdrumonkhan48519@gmail.com>
2022-04-30 11:35:10 +0600

    Login ui done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 
 function App() {
   
@@ -10,6 +11,9 @@ function App() {
      <Header></Header>
      <Routes>
        <Route path='/' element={<Home></Home>}></Route>
+       <Route path='/home' element={<Home></Home>}></Route>
+       <Route path='/login' element={<Login></Login>}></Route>
+
      </Routes>
     </div>
   );
