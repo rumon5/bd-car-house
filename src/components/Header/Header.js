99e9cd4f5bf07fa19d4944a0699be9bed0f09582
rumon5 <mdrumonkhan48519@gmail.com>
2022-04-30 13:06:22 +0600

    Register form ui done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav className='flex justify-between flex-wrap px-7 text-slate-900 py-3 bg-slate-100'>
+        <nav className='flex justify-between flex-col md:flex-row px-7 text-slate-900 py-3 bg-slate-100'>
             <div>
                 <h3>LOGO</h3>
             </div>
-            <div className='flex flex-wrap'>
+            <div className='flex flex-col md:flex-row'>
                 <Link className='mr-2' to='/home'>Home</Link>
                 <Link className='mr-2' to='/inventory'>Inventory</Link>
                 <Link className='mr-2' to='/dealers'>Dealers</Link>
@@ -18,6 +18,7 @@ const Header = () => {
                 {/* <Link to='/logout'>LOGOUT</Link> */}
             </div>
         </nav>
+        
     );
 };
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,10 +5,10 @@ const Login = () => {
     return (
         <div>
             <div className="block mx-auto my-5 p-6 rounded-lg shadow-lg bg-white max-w-sm">
-  <form>
-    <div className="form-group mb-6">
-      <label for="exampleInputEmail2" className="form-label inline-block mb-2 text-gray-700">Email address</label>
-      <input type="email" className="form-control
+                <form>
+                    <div className="form-group mb-6">
+                        <label htmlFor="exampleInputEmail2" className="form-label inline-block mb-2 text-gray-700">Email address</label>
+                        <input type="email" className="form-control
         block
         w-full
         px-3
@@ -23,11 +23,11 @@ const Login = () => {
         ease-in-out
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInputEmail2"
-        aria-describedby="emailHelp" placeholder="Enter email" />
-    </div>
-    <div className="form-group mb-6">
-      <label for="exampleInputPassword2" className="form-label inline-block mb-2 text-gray-700">Password</label>
-      <input type="password" className="form-control block
+                            aria-describedby="emailHelp" placeholder="Enter email" />
+                    </div>
+                    <div className="form-group mb-6">
+                        <label htmlFor="exampleInputPassword2" className="form-label inline-block mb-2 text-gray-700">Password</label>
+                        <input type="password" className="form-control block
         w-full
         px-3
         py-1.5
@@ -41,22 +41,22 @@ const Login = () => {
         ease-in-out
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInputPassword2"
-        placeholder="Password" />
-    </div>
-    <div className="flex justify-between items-center mb-6">
-      <div className="form-group form-check">
-        <input type="checkbox"
-          className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm 
+                            placeholder="Password" />
+                    </div>
+                    <div className="flex justify-between items-center mb-6">
+                        <div className="form-group form-check">
+                            <input type="checkbox"
+                                className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm 
           bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200
            mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-          id="exampleCheck2" />
-        <label className="form-check-label inline-block text-gray-800" for="exampleCheck2">Remember me</label>
-      </div>
-      <Link to="#!"
-        className="text-blue-600 hover:text-blue-700 focus:text-blue-700 transition duration-200 ease-in-out">Forgot
-        password?</Link>
-    </div>
-    <button type="submit" className="
+                                id="exampleCheck2" />
+                            <label className="form-check-label inline-block text-gray-800" htmlFor="exampleCheck2">Remember me</label>
+                        </div>
+                        <Link to="#!"
+                            className="text-blue-600 hover:text-blue-700 focus:text-blue-700 transition duration-200 ease-in-out">Forgot
+                            password?</Link>
+                    </div>
+                    <button type="submit" className="
       w-full
       px-6
       py-2.5
@@ -74,11 +74,11 @@ const Login = () => {
       transition
       duration-150
       ease-in-out">Sign in</button>
-    <p className="text-gray-800 mt-6 text-center">Not a member? <Link to="#!"
-        className="text-blue-600 hover:text-blue-700 focus:text-blue-700 transition duration-200 ease-in-out">Register</Link>
-    </p>
-  </form>
-</div>
+                    <p className="text-gray-800 mt-6 text-center">Not a member? <Link to="/register"
+                        className="text-blue-600 hover:text-blue-700 focus:text-blue-700 transition duration-200 ease-in-out">Register</Link>
+                    </p>
+                </form>
+            </div>
         </div>
     );
 };
