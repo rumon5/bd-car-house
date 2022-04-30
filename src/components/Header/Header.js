94f8e63376366bcae49ee81886094eeec562af63
rumon5 <mdrumonkhan48519@gmail.com>
2022-04-30 11:05:07 +0600

    Navbar done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <nav className='flex justify-between flex-wrap px-7 text-slate-900 py-3 bg-slate-100'>
+            <div>
+                <h3>LOGO</h3>
+            </div>
+            <div className='flex flex-wrap'>
+                <Link className='mr-2' to='/home'>Home</Link>
+                <Link className='mr-2' to='/inventory'>Inventory</Link>
+                <Link className='mr-2' to='/dealers'>Dealers</Link>
+                <Link className='mr-2' to='/contact'>Contact</Link>
+            </div>
+            <div>
+                <Link to='/login'>LogIn</Link>
+                {/* <Link to='/logout'>LOGOUT</Link> */}
+            </div>
+        </nav>
     );
 };
 
