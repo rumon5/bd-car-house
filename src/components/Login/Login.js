b49b0fc1895bd98aae3f50f21bd4ae974abf7d51
rumon5 <mdrumonkhan48519@gmail.com>
2022-05-01 16:45:50 +0600

    added facebook sign in on the login section

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,6 +7,7 @@ import auth from '../../firebase.init';
 const Login = () => {
     const navigate = useNavigate();
     const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+    const [signInWithFacebook, facebookUser, facebookLoading, facebookError] = useSignInWithFacebook(auth);
     
     const [
         signInWithEmailAndPassword,
@@ -39,7 +40,9 @@ const Login = () => {
                             Login To Your Account
                         </div>
                         <div className="flex gap-4 item-center">
-                            <button type="button" className="py-2 px-4 flex justify-center items-center  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                            <button 
+                            onClick={()=>signInWithFacebook()}
+                            type="button" className="py-2 px-4 flex justify-center items-center  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
                                 <svg width="20" height="20" fill="currentColor" className="mr-2" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M1343 12v264h-157q-86 0-116 36t-30 108v189h293l-39 296h-254v759h-306v-759h-255v-296h255v-218q0-186 104-288.5t277-102.5q147 0 228 12z">
                                     </path>
