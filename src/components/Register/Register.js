bda101cbb23f8409fc59f6573e7ed6d24e1d95e8
rumon5 <mdrumonkhan48519@gmail.com>
2022-05-01 16:20:09 +0600

    added google sign in on the register section

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom';
-import { useCreateUserWithEmailAndPassword, useSendEmailVerification } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useSendEmailVerification, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import Loading from '../Loading/Loading';
 import auth from '../../firebase.init';
 import toast from 'react-hot-toast';
@@ -9,6 +9,7 @@ import toast from 'react-hot-toast';
 const Register = () => {
     const navigate = useNavigate();
     const [sendEmailVerification, sending, verificationError] = useSendEmailVerification(auth);
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
     const [
         createUserWithEmailAndPassword,
         user,
@@ -19,7 +20,10 @@ const Register = () => {
     if (loading) {
         return <Loading></Loading>
     }
-    console.log(user);
+    if(googleUser){
+        navigate('/')
+    }
+
     if(user?.user?.emailVerified === false){
         navigate('/verifyEmail')
     }
@@ -54,7 +58,9 @@ const Register = () => {
                         </svg>
                         Facebook
                     </button>
-                    <button type="button" className="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                    <button 
+                    onClick={()=> signInWithGoogle()}
+                    type="button" className="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
                         <svg width="20" height="20" fill="currentColor" className="mr-2" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                             <path d="M896 786h725q12 67 12 128 0 217-91 387.5t-259.5 266.5-386.5 96q-157 0-299-60.5t-245-163.5-163.5-245-60.5-299 60.5-299 163.5-245 245-163.5 299-60.5q300 0 515 201l-209 201q-123-119-306-119-129 0-238.5 65t-173.5 176.5-64 243.5 64 243.5 173.5 176.5 238.5 65q87 0 160-24t120-60 82-82 51.5-87 22.5-78h-436v-264z">
                             </path>
