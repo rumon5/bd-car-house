f931d88dcf3a1921d20a2bf0a018fb499ebe0b05
rumon5 <mdrumonkhan48519@gmail.com>
2022-05-09 23:23:03 +0600

    fixed some issue

--- src/components/VerifyEmail/VerifyEmail.js
+++ src/components/VerifyEmail/VerifyEmail.js
@@ -1,12 +1,11 @@
-import { useAuthState } from 'react-firebase-hooks/auth';
-import auth from '../../firebase.init';
+import React from 'react';
 
 const VerifyEmail = () => {
-    const [user] = useAuthState(auth);
+    
         
     return (
         <div>
-            <h1 className='text-red-800 text-xl text-center m-9'>Please Verify Your Email and then reload the page!</h1>
+            <h1 className='text-red-800 text-xl text-center m-9'>Please Verify Your Email and then reload the page and go to another page!</h1>
         </div>
     );
 };
